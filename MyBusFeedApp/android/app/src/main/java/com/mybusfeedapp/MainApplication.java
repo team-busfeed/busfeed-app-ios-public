835a1f47603af484bb6271517001396d0e2958d9
Jack Phan <jack.phan.2018@sis.smu.edu.sg>
2021-01-18 23:58:10 +0800

    Android fix for BLE

--- MyBusFeedApp/android/app/src/main/java/com/mybusfeedapp/MainApplication.java
+++ MyBusFeedApp/android/app/src/main/java/com/mybusfeedapp/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.polidea.reactnativeble.BlePackage;
+import com.polidea.reactnativeble.BlePackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -12,6 +14,7 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.airbnb.android.react.maps.MapsPackage;
+
 
 
 public class MainApplication extends Application implements ReactApplication {
