2f9e4273412cf5be10588e252fbf2779e3591936
Emmanuel Rayendra <emmanuelr.2018@sis.smu.edu.sg>
2021-01-28 15:24:38 +0800

    UI Changes to Map Callouts

--- MyBusFeedApp/src/Components/Controls/Map.js
+++ MyBusFeedApp/src/Components/Controls/Map.js
@@ -1,13 +1,39 @@
 import React, { useState, Component } from 'react'
 import { View, StyleSheet, Text } from 'react-native'
-import MapView from 'react-native-maps'
+import MapView, {Marker, Callout} from 'react-native-maps'
 import { default as Navigation } from './Navigation'
 import tailwind from 'tailwind-rn'
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+// import CustomCallout from './CustomCallout'
+// import { Icon } from 'react-native-vector-icons/Icon'
+
 
 const styles = StyleSheet.create({
     nav: {
         marginTop: 20
     },
+    callout: {
+        backgroundColor: "white",
+        borderRadius: 4,
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 10, 
+      },
+    title: {
+        color: "#4F4F4F",
+        fontSize: 14,
+        lineHeight: 18,
+        flex: 1,
+        fontWeight: "bold"
+    },
+    description: {
+        color: "#707070",
+        fontSize: 12,
+        lineHeight: 16,
+        flex: 1,
+    }
+
 });
 class Map extends Component {
     
@@ -17,15 +43,34 @@ class Map extends Component {
             markers = <Text>Maps loading..</Text>
         } else {
             markers = this.props.states.busStops.map(busStop => 
-                <MapView.Marker
+                <Marker
                     key={busStop.busstop_number}
                     coordinate={{latitude: busStop.busstop_lat ? Number(busStop.busstop_lat) : 0, longitude: busStop.busstop_lng ? Number(busStop.busstop_lng) : 0}}
                     title={busStop.busstop_name}
                     description={"Stop Number: " + busStop.busstop_number}
-                />
+                >
+                    <Callout
+                    tooltip={true}
+                    style={styles.callout}
+                    >
+                        <View style={tailwind('flex flex-row')}>
+                            <View style={tailwind('w-1/5')}>
+                                <Icon name="bus" size={20} color="black" style={[tailwind("my-2"), styles.image]}/>
+                            </View>
+                            <View style={tailwind('w-4/5')}>
+                                <Text style={styles.title}>
+                                    {busStop.busstop_name}
+                                </Text>
+                                <Text style={styles.description}>
+                                    {"Stop Number: " + busStop.busstop_number}
+                                </Text>
+                            </View>
+                        </View>
+                  </Callout>
+                </Marker>
             )
         }
-
+        
         return (
             <View>
                 <MapView
