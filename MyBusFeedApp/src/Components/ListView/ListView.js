cbd4d87365fd1f90cc3be19bbc72b837d2ee5dca
Emmanuel Rayendra <emmanuelr.2018@sis.smu.edu.sg>
2021-01-31 23:39:07 +0800

    [BFT-87] Refresh current location & nearby bus stops feature

--- MyBusFeedApp/src/Components/ListView/ListView.js
+++ MyBusFeedApp/src/Components/ListView/ListView.js
@@ -12,26 +12,108 @@ class ListView extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      latitude: 1.3521,
-      longitude: 103.8198,
+      latitude: this.props.states.latitude,
+      longitude: this.props.states.longitude,
       userProximity: false,
-      busService: [],
+      isLoading: false,
+      busStops: this.props.states.busStops
     }
   }
 
-  render() {
-    return (
-      <View style={tailwind('h-64 bg-white px-2 pb-7')}>
-        <FlatList
-          data={this.props.states.busStops}
-          renderItem={({ item }) => (
-            <Accordion title={item} data={item} />
-          )}
-          keyExtractor={(item) => item.busstop_number}
-        />
-      </View>
-    )
-  }
+    getGeoLocation() {
+        Geolocation.getCurrentPosition((info) => {
+            console.log("========================")
+            console.log("Got current geolocation!")
+            console.log("========================")
+            this.setState({
+                latitude: info.coords.latitude,
+                longitude: info.coords.longitude,
+                latitudeDelta: 0.005,
+                longitudeDelta: 0.005,
+                updatedGeolocation: true,
+            })
+
+            console.log("LAT:" + info.coords.latitude)
+            console.log("LONG:" + info.coords.longitude)
+            console.log(this.state.updatedGeolocation ? "Updated to real-time geolocation values!" : "Using default geolocation values")
+
+
+            this.getProximityBusStops()
+        }, (error) => console.log('position error!!!', error),
+        {enableHighAccuracy: true, timeout: 20000, maximumAge: 0})
+    }
+
+    getProximityBusStops() {
+        this.props.states.busStops = []
+        console.log("========================")
+        console.log('Getting data from Location Service API!!')
+        console.log("========================")
+        const fetchURL = "https://api.mybusfeed.com/location/getListOfBusStopNo/".concat(this.state.latitude, "-", this.state.longitude)
+        axios
+        .get(fetchURL)
+        .then((response) => {
+            console.log("Fetched API data: " + JSON.stringify(response.data))
+
+            if (response.data.status === "not_found") {
+                this.props.states.busStops = []
+                this.setState({
+                    isLoading: false
+                })
+            } else {
+                this.props.states.busStops = response.data
+                this.setState({
+                    isLoading: false
+                })
+            }
+        })
+        .catch((error) => {
+            console.log('error:', error)
+        })
+    }
+
+
+    render() {
+        flatList = null
+        
+        if (this.props.states.isLoading) {
+            loading = 
+            <View style={tailwind('px-4 my-4')}>
+                <Text style={tailwind('text-lg text-center')}>Loading data...</Text>
+            </View>
+        } else {
+
+            if (this.props.states.busStops.length == 0) {
+                localVarBusStops = [{"type": "-1", "message": "No nearby bus stops."}]
+                flatList = <FlatList
+                data={localVarBusStops}
+                onRefresh={() => this.getGeoLocation()}
+                refreshing={this.state.isLoading}
+                renderItem={({ item }) => (
+                    <View style={tailwind('px-4 my-4')}>
+                        <Text style={tailwind('text-lg text-center')}>{item.message}</Text>
+                    </View>
+                )}
+                keyExtractor={(item) => item.type}
+                />
+            } else {
+                flatList = <FlatList
+                data={this.props.states.busStops}
+                onRefresh={() => this.getGeoLocation()}
+                refreshing={this.state.isLoading}
+                renderItem={({ item }) => (
+                    <Accordion title={item} data={item} />
+                )}
+                keyExtractor={(item) => item.busstop_number}
+                />
+            }
+        }
+
+        return (
+        <View style={tailwind('h-64 bg-white px-2 pb-7')}>
+            {flatList}
+        </View>
+        )
+    }
 }
 
 export default ListView
