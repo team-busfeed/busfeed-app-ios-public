359147945e1b441a4ae089f18afe63953a2d2815
Emmanuel Rayendra <emmanuelr.2018@sis.smu.edu.sg>
2021-02-01 13:08:36 +0800

    [BFT-47] Implemented Search UI

--- MyBusFeedApp/src/Components/Controls/Header.js
+++ MyBusFeedApp/src/Components/Controls/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native'
 import tailwind from 'tailwind-rn'
 import { WebView } from 'react-native-webview'
@@ -11,26 +11,63 @@ const styles = StyleSheet.create({
     },
 });
 
-const Header = () => {
-  
-    return (
-        <View style={tailwind('m-3 bg-white')}>
+export default class Header extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isSearchInputHidden: true,
+            searchText: ""
+        }
+    }
+
+    didPressSearchButton() {
+        console.log("=======================")
+        console.log("Did press search button")
+        console.log("=======================")
+
+        this.setState((previousState) => ({
+            isSearchInputHidden: !previousState.isSearchInputHidden,
+        }))
+    }
+
+    onChangeText(text) {
+        console.log(text)
+    }
+
+    render() {
+        controls = null
+        
+        if (this.state.isSearchInputHidden) {
+            controls =  
             <View style={tailwind('flex flex-row')}>
                 <View style={tailwind('w-64')}>
                     <Text style={tailwind('text-xl font-semibold text-gray-600 mx-2')}>
-                        Bus Locator
+                        MyBusFeed
                     </Text>
                 </View>
                 <View style={tailwind('w-1/5')}>
-                    <Icon name="search" size={20} color="grey"/>
+                    <Icon name="search" size={20} color="grey" onPress={() => this.didPressSearchButton()}/>
                 </View>
                 <View style={tailwind('w-1/5')}>
-                <Icon name="ellipsis-v" size={20} color="grey"/>
+                    <Icon name="ellipsis-v" size={20} color="grey"/>
                 </View>
             </View>
-            
-        </View>
-    )
+        } else {
+            controls = <View style={tailwind('flex flex-row')}>
+                <View style={tailwind('w-5/6')}>
+                    <TextInput
+                        style={tailwind('h-8 border border-gray-400 rounded-2xl px-3')}
+                    />
+                </View>
+                <View style={tailwind('w-1/6 p-1 ml-2 mt-2')}>
+                    <Text style={tailwind('text-blue-500')} onPress={() => this.didPressSearchButton()}>Cancel</Text>
+                </View>
+            </View>
+        }
+        return (
+            <View style={tailwind('m-3 bg-white')}>
+                {controls}
+            </View>
+        )
+    }
 }
-  
-export default Header
