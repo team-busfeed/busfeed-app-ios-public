40f5db3fb525a78409fa86327b320e6e0a3fd5ae
Emmanuel Rayendra <emmanuelr.2018@sis.smu.edu.sg>
2021-04-20 11:19:10 +0800

    Fixed: Blue icon for location on start

--- BusfeedApp/src/Components/Controls/Navigation.js
+++ BusfeedApp/src/Components/Controls/Navigation.js
@@ -31,10 +31,10 @@ export default class Navigation extends Component {
         return (
             <View style={tailwind('flex flex-row h-full')}>
                 <TouchableOpacity onPress={() => this.didSelectNav(0)} style={tailwind('w-1/2 pt-5')}>
-                    <Icon name="location-pin" size={30} style={this.props.states.selected == 0 ? tailwind('self-center text-blue-500') : tailwind('self-center text-gray-600')} onPress={() => this.didSelectNav(0)}/>
+                    <Icon name="location-pin" size={30} style={this.props.states.selected != 1 ? tailwind('self-center text-blue-500') : tailwind('self-center text-gray-600')} onPress={() => this.didSelectNav(0)}/>
                 </TouchableOpacity>
                 <TouchableOpacity onPress={() => this.didSelectNav(1)} style={tailwind('w-1/2 pt-5')}>
-                    <Icon name="favorite" size={30} style={this.props.states.selected ? tailwind('self-center text-red-500') : tailwind('self-center text-gray-600')} onPress={() => this.didSelectNav(1)}/>
+                    <Icon name="favorite" size={30} style={this.props.states.selected == 1 ? tailwind('self-center text-red-500') : tailwind('self-center text-gray-600')} onPress={() => this.didSelectNav(1)}/>
                 </TouchableOpacity>
             </View> 
         )
